4cb69fd1286bfdd277b40123d37891fc5c347b8c
Dylan Davy <dylandavy01@gmail.com>
2021-02-25 12:18:48 +1300

    Created basic files, icons and titles

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.App}>
-      
+      <h2>This webpage will be automatically updated when changes are made to the main branch on <a href="https://github.com/Dylan-Davy/portfolio">Github</a>.</h2>
     </div>
   );
 }
